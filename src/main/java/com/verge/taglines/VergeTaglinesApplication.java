4a84f411f0cf4574573e99e24d3fd1becaef834b
Tyler Carberry <tyler.carberry@gmail.com>
2022-09-23 21:50:33 -0400

    update dependencies

--- src/main/java/com/verge/taglines/VergeTaglinesApplication.java
+++ src/main/java/com/verge/taglines/VergeTaglinesApplication.java
@@ -1,6 +1,5 @@
 package com.verge.taglines;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VergeTaglinesApplication {
-
-	@Value("${TARGET:World}")
-	String target;
 
 	@RestController
 	static class MainController {
